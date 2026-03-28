4e9bda0e2a0b2ed5ea7901a1936a05a871909af5
rihabcherni <rihabcherni000@gmail.com>
2026-03-28 19:09:39 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css';
 import React, { useState, useEffect } from 'react';
 import Navbar from './components/Nav';
 import Hero from './components/Hero';
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
 import ReactGA from "react-ga4";
+import appCssUrl from './App.css?url';
 
 ReactGA.initialize("G-8L8QX5MLTQ"); 
 
 ReactGA.send("pageview");
+
+const cssLink = document.createElement('link');
+cssLink.rel = 'stylesheet';
+cssLink.href = appCssUrl;
+cssLink.media = 'print';
+cssLink.onload = () => {
+  cssLink.media = 'all';
+};
+document.head.appendChild(cssLink);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>

f3ed5994ea246b92146881b631eeccabc8295523
rihabcherni <rihabcherni000@gmail.com>
2026-03-28 19:39:17 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './App.css';
+import './index.css';
 import App from './App.jsx'
-import appCssUrl from './App.css?url';
 
 const initAnalytics = () => {
   import("react-ga4").then(({ default: ReactGA }) => {
@@ -15,15 +16,6 @@ if ('requestIdleCallback' in window) {
 } else {
   window.setTimeout(initAnalytics, 1500);
 }
-
-const cssLink = document.createElement('link');
-cssLink.rel = 'stylesheet';
-cssLink.href = appCssUrl;
-cssLink.media = 'print';
-cssLink.onload = () => {
-  cssLink.media = 'all';
-};
-document.head.appendChild(cssLink);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
